d0519cf96234350641f128e060d5da1dc993f76c
Brent Westbrook <brentrwestbrook@gmail.com>
2024-07-24 15:10:35 -0400

    require -f flag for ff, better error handling, allow built-in ff

--- scripts/general.sh
+++ scripts/general.sh
@@ -1,7 +1,5 @@
 set -e
 
-ff=$1
-shift
 ncpus=48
 hours=84
 mem=32
@@ -10,23 +8,53 @@ dataset=industry
 
 cmd=sbatch
 
-while getopts "dhc:t:m:s:e:" arg; do
+usage="Usage: $0 -f FORCEFIELD [-c CPUS] [-t CPU_HOURS] [-m GB_MEMORY] [-h]"
+
+case $# in
+	0) echo 'error: no arguments provided'
+	   echo $usage
+	   exit 1;;
+esac
+
+while getopts "dhc:t:m:s:e:f:" arg; do
 	case $arg in
-		h) echo 'usage: [-c CPUS] [-t CPU_HOURS] [-m GB_MEMORY] [-h]' ;;
+		h) echo $usage
+		   exit 0;;
 		c) ncpus=$OPTARG ;;
 		t) hours=$OPTARG ;;
 		m) mem=$OPTARG ;;
 		s) dataset=$OPTARG ;;
 		d) cmd=cat ;; # dry run
 		e) env=$OPTARG ;; # conda env
+		f) ff=$OPTARG ;;
 	esac
 done
+
+if [[ -z "$ff" ]]
+then
+	echo 'error: no force field provided'
+	echo $usage
+	exit 1
+fi
+
+# allow providing just the base of the ff filename if it's present in the
+# forcefields directory; otherwise, arrange to pass this exact path to the
+# toolkit and extract the base of the filename for the output dir and name of
+# the sqlite file
+if [[ -f forcefields/$ff.offxml ]]
+then
+	ffpath=forcefields/$ff.offxml
+else
+	ffpath=$ff
+	ffbase=$(basename $ff)
+	ff=${ffbase%.offxml}
+fi
 
 echo generating input for force field $ff, with $ncpus cpus, $mem gb, and $hours hours
 
 $cmd <<INP
 #!/bin/bash
-#SBATCH -J ib-$ff
+#SBATCH -J bench-$ff
 #SBATCH -p standard
 #SBATCH -t $hours:00:00
 #SBATCH --nodes=1
@@ -47,7 +75,7 @@ mamba activate $env
 echo \$OE_LICENSE
 
 python -u main.py \
-       --forcefield forcefields/$ff.offxml \
+       --forcefield $ffpath \
        --dataset datasets/cache/$dataset.json \
        --sqlite-file $ff.sqlite \
        --out-dir output/$dataset/$ff \
