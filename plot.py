4c7fc861b382d7c9b922671587c00962c95d7608
Brent Westbrook <brentrwestbrook@gmail.com>
2024-07-26 13:06:05 -0400

    delete non-plotting code

--- plot.py
+++ plot.py
@@ -1,46 +1,8 @@
 from pathlib import Path
 
 import click
-import numpy as np
-import pandas as pd
 
 from main import plot
-
-pd.set_option("display.max_columns", None)
-
-
-def load_csvs(dir: Path):
-    """Load the DDE, RMSD, and TFD CSV files in `dir` and return the merged
-    dataframe"""
-    dde = pd.read_csv(dir / "dde.csv")
-    dde.columns = ["rec_id", "dde"]
-    rmsd = pd.read_csv(dir / "rmsd.csv")
-    rmsd.columns = ["rec_id", "rmsd"]
-    tfd = pd.read_csv(dir / "tfd.csv")
-    tfd.columns = ["rec_id", "tfd"]
-    return dde.merge(rmsd).pipe(pd.DataFrame.merge, tfd)
-
-
-def stats(dirs, out):
-    res = []
-    for d in dirs:
-        d = Path(d)
-        df = load_csvs(d)
-        res.append((d.name, df))
-
-    for m in ["dde", "rmsd", "tfd"]:
-        for n, df in res:
-            data = df[df[m].notnull()][m]
-            avg = np.mean(data)
-            mae = np.mean(np.abs(data))
-            mdn = np.median(data)
-            std = np.std(data)
-            o = m.upper()
-            print(
-                f"{n}&{o}& {avg:.2f} & {mae:.2f} & {mdn:.2f} & {std:.2f} \\\\",
-                file=out,
-            )
-        print("\\hline", file=out)
 
 
 def plotter(ffs, output_dir, input_dir="industry", names=None, **kwargs):
@@ -50,8 +12,6 @@ def plotter(ffs, output_dir, input_dir="industry", names=None, **kwargs):
     out_path = Path(output_dir)
     out_path.mkdir(exist_ok=True)
     plot(output_dir, dirs, names, **kwargs)
-    with open("current/tabs/stats.tex", "w") as out:
-        stats(dirs, out)
 
 
 @click.command()
